bae5cbc3370f8506e06210a61892e94b0421a821
Karl Pfister <pfister@Karls-MacBook-Pro.local>
2018-12-12 11:42:51 -0700

    Create Models

---
+++ CoreDataStack.swift
@@ -0,0 +1,27 @@
+//
+//  CoreDataStack.swift
+//  PlaylistCoreData
+//
+//  Created by Karl Pfister on 12/12/18.
+//  Copyright © 2018 Karl Pfister. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "PlaylistCoreData")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error{
+                fatalError("Failed to Load Persistent Store \(error)")
+            }
+        })
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext{
+        return container.viewContext
+    }
+}
